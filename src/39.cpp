7456d1c713c4a0803a5d75625109609516f7c995
fiorioilia59 <fiorioilia59@gmail.com>
2025-05-23 07:46:53 +0000

    Add 39.cpp

---
+++ src/39.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int z = x + y; // Addition
+    if (z > 20) {
+        cout << "The sum is: " << z << endl;
+    }
+    else {
+        cout << "The sum is less than or equal to 20." << endl;
+    }
+
+    return 0;
+}
